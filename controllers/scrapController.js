264da8ee7947a3dcafacc2d9b9c405677be94bdb
Cleiton Correa <cleitoncorrea2013@hotmail.com>
2022-05-08 21:15:22 -0300

    Add cron scraping page

--- controllers/scrapController.js
+++ controllers/scrapController.js
@@ -58,4 +58,42 @@ async function scrapeLogin(browserInstance) {
     }
 }
 
+async function scrapingCron(callback, startTime, interval, threshold) {
+    function callbackWithTimeout() {
+        var timeout =
+            interval === undefined ? null : setTimeout(callbackWithTimeout, interval);
+        callback(timeout);
+    }
+    if (startTime === undefined) {
+        // Corre em intervalos a partir do próximo ciclo de eventos
+        return setTimeout(callbackWithTimeout, 0);
+    }
+    // Limitar startTime a hora de um dia
+    startTime %= 86400000;
+    var currentTime = new Date() % 86400000;
+    if (interval === undefined) {
+        // Corre uma vez
+        // Se startTime é no passado, corre no próximo ciclo de eventos
+        // Senão, espera o tempo suficiente
+        return setTimeout(
+            callbackWithTimeout,
+            Math.max(0, startTime - currentTime)
+        );
+    } else {
+        var firstInterval = (startTime - currentTime) % interval;
+        if (firstInterval < 0) firstInterval += interval;
+        // Se falta mais do que threshold para a próxima hora,
+        // corre no próximo ciclo de eventos, agenda para a próxima hora
+        // e depois continua em intervalos
+        if (threshold === undefined || firstInterval > threshold) {
+            return setTimeout(function() {
+                var timeout = setTimeout(callbackWithTimeout, firstInterval);
+                callback(timeout);
+            }, 0);
+        }
+        // Senão, começa apenas na próxima hora e continua em intervalos
+        return setTimeout(callbackWithTimeout, firstInterval);
+    }
+}
+
 module.exports = (browserInstance) => scrapeAll(browserInstance);
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const browserObject = require("../browser.js");
 const scraperController = require("../controllers/scrapController");
+const dispacthScrapingPage;
+const alertCronDispatch = [];
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -10,9 +12,28 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/login", function(req, res, next) {
+
+    let email = req.body.name;
+    let password = req.body.password;
+    let valor = req.body.valor;
+    let msg = req.body.msg;
     //Start the browser and create a browser instance
     let browserInstance = browserObject.startBrowser();
     // Pass the browser instance to the scraper controller
+    for (let i = 0; i <= 60; i = i + 5) {
+        alertCronDispatch.push(
+            new Cron.Job(i + " * * * *", function() {
+                dispacthScrapingPage = true;
+            })
+        );
+    }
+
+    if (dispacthScrapingPage) {
+        //executa a varredura na página
+        scraperController(browserInstance);
+    }
+
+    //Verificar Status da página
     scraperController(browserInstance);
 });
 
